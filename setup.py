719f1ff68ead2674446973ba80f6a60236e90eac
Arsal Azeem <azeemarsal@gmail.com>
2021-01-21 17:23:15 +0500

    ---

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='generate_test_data',
+      version='1.0',
+      description='Package To Generate Test Data for Testing/QA',
+      author='Arsal Azeem',
+      author_email='gward@python.net',
+      url='https://www.python.org/sigs/distutils-sig/',
+      packages=['distutils', 'distutils.command'],
+     )
